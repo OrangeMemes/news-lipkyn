1e362fc8f53093cb33963ecfebc37ffd2159a102
Vladislav <maxvlod@gmail.com>
2019-04-26 00:20:45 +0300

    basic news display

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo 'hello world';
+    require ('init.php')
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Новостной портал</title>
+    <script src="https://code.jquery.com/jquery-1.11.3.js"></script>
+</head>
+<body>
+<header>
+<a>админка</a>
+</header>
+
+<?php
+
+$news_list = $conn->query("SELECT `title`, `text`, `postingTime` FROM news ORDER BY `postingTime`")->fetch_all(MYSQLI_ASSOC);
+foreach ($news_list as $news_item) {
+    echo '<article>'.'<h2>'.$news_item["title"].'</h2>'.'<p>'.$news_item["text"].'</p>'.'</article>';
+}
+
+?>
+</body>
